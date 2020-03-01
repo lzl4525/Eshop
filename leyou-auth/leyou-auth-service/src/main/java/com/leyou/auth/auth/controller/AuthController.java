5fa0d34faf775c03c709bdb5cad6351b28e20ecb
zl4525 <lzl4525@163.com>
2020-03-01 15:07:22 +0800

    权限控制微服务，工具包，鉴权中心

---
+++ leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/auth/controller/AuthController.java
@@ -0,0 +1,75 @@
+package com.leyou.auth.auth.controller;
+
+import com.leyou.auth.auth.service.AuthService;
+import com.leyou.auth.config.JwtProperties;
+import com.leyou.common.pojo.UserInfo;
+import com.leyou.common.utils.CookieUtils;
+import com.leyou.common.utils.JwtUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@EnableConfigurationProperties(JwtProperties.class)
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @PostMapping("accredit")
+    public ResponseEntity<Void> accredit(
+            @RequestParam("username")String username,
+            @RequestParam("password")String password,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+        String token = this.authService.accredit(username,password);
+
+        if(StringUtils.isBlank(token)){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        CookieUtils.setCookie(request,response, this.jwtProperties.getCookieName(), token, this.jwtProperties.getExpire()*60);
+
+        return ResponseEntity.ok(null);
+    }
+    
+    @GetMapping("verify")
+    public ResponseEntity<UserInfo> verify(
+            @CookieValue("LY_TOKEN")String token,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+
+        try {
+            //通过工具类解析cookie
+            UserInfo user = JwtUtils.getInfoFromToken(token, this.jwtProperties.getPublicKey());
+            if (user == null){
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+            //刷新jwt的有效时间
+            JwtUtils.generateToken(user, this.jwtProperties.getPrivateKey(), this.jwtProperties.getExpire());
+
+            //刷新cookie的时间
+            CookieUtils.setCookie(request,response, this.jwtProperties.getCookieName(), token, this.jwtProperties.getExpire()*60);
+
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+}
