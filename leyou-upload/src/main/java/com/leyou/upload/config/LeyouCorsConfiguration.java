2c9d3c2f489c1930a75665d0275d7997d45720f4
zl4525 <lzl4525@163.com>
2020-03-01 14:53:57 +0800

    image上传微服务，连接fastDFS存储图片

---
+++ leyou-upload/src/main/java/com/leyou/upload/config/LeyouCorsConfiguration.java
@@ -0,0 +1,33 @@
+package com.leyou.upload.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class LeyouCorsConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter(){
+
+        //初始化cors配置对象
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //允许跨域的域名
+        configuration.addAllowedOrigin("http://manage.leyou.com");
+        configuration.setAllowCredentials(true);  //允许携带cookie
+        configuration.addAllowedMethod("*");   //代表所有方法
+        configuration.addAllowedHeader("*");  //允许携带任何头信息
+
+        //初始化cors配置源对象
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**",configuration);
+
+        //返回corsFilter实例，参数:cors配置元对象
+        return new CorsFilter(configurationSource);
+    }
+
+}
