10d60cc6c7a904d245c0ce24b5b2cb4d8ab21a07
zl4525 <lzl4525@163.com>
2020-03-01 15:02:42 +0800

    购物车微服务，离线状态购物车，在线状态购物车

---
+++ leyou-cart/src/main/java/com/leyou/cart/interceptor/LoginInterceptor.java
@@ -0,0 +1,49 @@
+package com.leyou.cart.interceptor;
+
+import com.leyou.cart.config.JwtProperties;
+import com.leyou.common.pojo.UserInfo;
+import com.leyou.common.utils.CookieUtils;
+import com.leyou.common.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@EnableConfigurationProperties(JwtProperties.class)
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    private static final ThreadLocal<UserInfo> THREAD_LOCAL = new ThreadLocal<>();
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取cookie中的token
+        String token = CookieUtils.getCookieValue(request, this.jwtProperties.getCookieName());
+        //解析token，获取用户信息
+        UserInfo userInfo = JwtUtils.getInfoFromToken(token, this.jwtProperties.getPublicKey());
+        if(userInfo == null){
+            return false;
+        }
+        //把userinfo放入线程局部变量
+        THREAD_LOCAL.set(userInfo);
+
+        return true;
+    }
+
+    public static UserInfo getUserInfo(){
+        return THREAD_LOCAL.get();
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        //清空线程局部变量   tomcat的线程池   线程不会结束，变量不会释放，需要手动释放
+        THREAD_LOCAL.remove();
+    }
+}
---
+++ leyou-cart/src/main/java/com/leyou/cart/service/CartService.java
@@ -0,0 +1,103 @@
+package com.leyou.cart.service;
+
+import com.leyou.cart.client.GoodsClient;
+import com.leyou.cart.interceptor.LoginInterceptor;
+import com.leyou.cart.pojo.Cart;
+import com.leyou.common.pojo.UserInfo;
+import com.leyou.common.utils.JsonUtils;
+import com.leyou.item.pojo.Sku;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CartService {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private GoodsClient goodsClient;
+
+    private static final String KEY_PREFIX = "user:cart";
+
+    public void addCart(Cart cart) {
+        //获取用户信息
+        UserInfo userInfo = LoginInterceptor.getUserInfo();
+        //查询购物车记录
+        BoundHashOperations<String, Object, Object> hashOperations = this.redisTemplate.boundHashOps(KEY_PREFIX + userInfo.getId());
+        String key = cart.getSkuId().toString();
+        Integer num = cart.getNum();
+        //判断当前商品是否在购物车
+        if(hashOperations.hasKey(key)){
+            //在  更新
+            String cartJson = hashOperations.get(key).toString();
+            cart = JsonUtils.parse(cartJson, Cart.class);
+            cart.setNum(cart.getNum() + num);
+            hashOperations.put(key, JsonUtils.serialize(cart));
+        }else {
+            Sku sku = this.goodsClient.querySkuBySkuId(cart.getSkuId());
+            //不再   新增
+            cart.setUserId(userInfo.getId());
+            cart.setTitle(sku.getTitle());
+            cart.setOwnSpec(sku.getOwnSpec());
+            cart.setImage(StringUtils.isBlank(sku.getImages()) ? "": StringUtils.split(sku.getImages(), ",")[0]);
+            cart.setPrice(sku.getPrice());
+        }
+        hashOperations.put(key, JsonUtils.serialize(cart));
+    }
+
+    public List<Cart> queryCarts() {
+        //获取用户信息
+        UserInfo userInfo = LoginInterceptor.getUserInfo();
+        //判断用户是否有购物车记录
+        if (!this.redisTemplate.hasKey(KEY_PREFIX + userInfo.getId())){
+            return null;
+        }
+
+        //获取用户购物车记录
+        BoundHashOperations<String, Object, Object> hashOperations = this.redisTemplate.boundHashOps(KEY_PREFIX + userInfo.getId());
+
+        List<Object> cartsJson = hashOperations.values();
+        //如果购物车集合为空  返回null
+        if(CollectionUtils.isEmpty(cartsJson)){
+            return null;
+        }
+
+        //把list<object>  集合转化未list<cart>集合
+        return cartsJson.stream().map(cartJson -> JsonUtils.parse(cartJson.toString(), Cart.class)).collect(Collectors.toList());
+
+    }
+
+    public void updateNum(Cart cart) {
+        //获取用户信息
+        UserInfo userInfo = LoginInterceptor.getUserInfo();
+        //判断用户是否有购物车记录
+        if (!this.redisTemplate.hasKey(KEY_PREFIX + userInfo.getId())){
+            return;
+        }
+        Integer num = cart.getNum();
+        //获取用户购物车记录
+        BoundHashOperations<String, Object, Object> hashOperations = this.redisTemplate.boundHashOps(KEY_PREFIX + userInfo.getId());
+
+        String cartJson = hashOperations.get(cart.getSkuId().toString()).toString();
+        cart = JsonUtils.parse(cartJson, Cart.class);
+
+        cart.setNum(num);
+        hashOperations.put(cart.getSkuId().toString(),JsonUtils.serialize(cart));
+    }
+
+    public void deleteCart(String skuId) {
+        // 获取登录用户
+        UserInfo user = LoginInterceptor.getUserInfo();
+        String key = KEY_PREFIX + user.getId();
+        BoundHashOperations<String, Object, Object> hashOps = this.redisTemplate.boundHashOps(key);
+        hashOps.delete(skuId);
+    }
+}
