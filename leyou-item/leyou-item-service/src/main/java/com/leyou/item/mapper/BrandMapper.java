080af68966a9df18a5202c023fb19a39740d730a
zl4525 <lzl4525@163.com>
2020-03-01 14:52:19 +0800

    后台管理   品牌、商品管理微服务

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,28 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface BrandMapper extends Mapper<Brand> {
+
+    /**
+     *
+     * @param cid
+     * @param bid
+     */
+    @Insert("INSERT INTO tb_category_brand (category_id, brand_id) values (#{cid}, #{bid})")
+    void insertCategoryAndBrand(@Param("cid") Long cid,@Param("bid") Long bid);
+
+    /**
+     * 根据分类ID查询品牌列表
+     * @param cid
+     * @return
+     */
+    @Select("SELECT * FROM tb_brand a INNER JOIN tb_category_brand b on a.id=b.brand_id where b.category_id=#{cid}")
+    List<Brand> selectBrandsByCid(Long cid);
+}
---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,61 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpecificationService {
+
+    @Autowired
+    private SpecGroupMapper groupMapper;
+
+    @Autowired
+    private SpecParamMapper paramMapper;
+
+    /**
+     * 根据分类ID查询分类组
+     * @param cid
+     * @return
+     */
+    public List<SpecGroup> queryGroupsByCid(Long cid) {
+        SpecGroup record = new SpecGroup();
+        record.setCid(cid);
+        return this.groupMapper.select(record);
+    }
+
+    /**
+     *根据条件查询规格参数
+     * @param gid
+     * @return
+     */
+    public List<SpecParam> queryParms(Long gid, Long cid, Boolean generic, Boolean searching) {
+
+        SpecParam record = new SpecParam();
+        record.setGroupId(gid);
+        record.setCid(cid);
+        record.setGeneric(generic);
+        record.setSearching(searching);
+        return this.paramMapper.select(record);
+    }
+
+    /**
+     * 根据id查询组及规格参数
+     * @param cid
+     * @return
+     */
+    public List<SpecGroup> queryGroupsWithParam(Long cid) {
+        List<SpecGroup> groups = this.queryGroupsByCid(cid);
+        groups.forEach(group -> {
+            List<SpecParam> params = this.queryParms(group.getId(), null, null, null);
+            group.setParams(params);
+        });
+        return groups;
+    }
+}
